0dfbc0fedfbfd9598b02af8ff2dd75c752c945ac
adam kuchto <adamkuchto@gmail.com>
2026-05-06 14:06:24 +0200

    add read file

--- UV Presets/src/main.cpp
+++ UV Presets/src/main.cpp
@@ -8,26 +8,9 @@ void setup()
   Serial.begin(115200);
   delay(1000);
 
-  SD_STAT status_sd;
-  status_sd = initSdCard(spiSD);
+  logger(initSdCard(spiSD));
+  logger(readFile());
 
-  switch (status_sd)
-  {
-  case SD_INIT:
-    Serial.println("START");
-    Serial.printf("Rozmiar: %llu MB\n\r", SD.cardSize() / (1024 * 1024));
-    break;
-  case SD_CARD_NONE:
-    Serial.println("BRAK KARTY");
-    break;
-  case SD_ERROR:
-    Serial.println("FAIL");
-    break;
-
-  default:
-    Serial.println("FAIL");
-    break;
-  }
 }
 
 void loop()
