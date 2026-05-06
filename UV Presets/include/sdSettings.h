d76b61c61ff552af2ef220bf7dff9f8bd7cd2a47
adam kuchto <adamkuchto@gmail.com>
2026-05-06 14:06:24 +0200

    add read file

--- UV Presets/include/sdSettings.h
+++ UV Presets/include/sdSettings.h
@@ -3,22 +3,40 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define SD_MISO  2
-#define SD_MOSI  3
-#define SD_SCLK  4
-#define SD_CS    7
+#define SD_MISO 2
+#define SD_MOSI 3
+#define SD_SCLK 4
+#define SD_CS 7
 #define SPI_SPEED_FOR_SD 4000000
+#define JSON_FILE "/settings.json"
 
-typedef enum {
+typedef enum
+{
     SD_INIT = 0,
     SD_ERROR = 1,
-    SD_CARD_NONE = 2
+    SD_CARD_NONE = 2,
+    SD_FILE_ERROR = 3,
+    SD_FILE_OK = 4
 } SD_STAT;
 
 /**
  * @brief Inicjalizuje połączenie SD przez SPI.
- * 
- * @param mainLine Magistrala SPI.
+ *
+ * @param[in][out] mainLine Magistrala SPI.
  * @return SD_STAT ENUM informacja o przebiegu połączenia.
  */
 SD_STAT initSdCard(SPIClass &mainLine);
+
+/**
+ * @brief Czyta plik z ustawieniami.
+ *
+ * @return SD_STAT ENUM informacja o przebiegu odczytu pliku.
+ */
+SD_STAT readFile();
+
+/**
+ * @brief Wyświetla komunikaty odnośnie dzałań na karcie SD.
+ *
+ * @param[in] status_sd Status zwracany przez funkcję.
+ */
+void logger(SD_STAT status_sd);
--- UV Presets/src/sdSettings.cpp
+++ UV Presets/src/sdSettings.cpp
@@ -16,3 +16,49 @@ SD_STAT initSdCard(SPIClass &mainLine)
 
     return SD_INIT;
 }
+
+SD_STAT readFile()
+{
+    File fileSettings = SD.open(JSON_FILE);
+
+    if (fileSettings)
+    {
+        while (fileSettings.available())
+        {
+            Serial.write(fileSettings.read());
+        }
+        fileSettings.close();
+    }
+    else
+    {
+        return SD_FILE_ERROR;
+    }
+    return SD_FILE_OK;
+}
+
+void logger(SD_STAT status_sd)
+{
+    switch (status_sd)
+    {
+    case SD_INIT:
+        Serial.println("START");
+        Serial.printf("Rozmiar: %llu MB\n\r", SD.cardSize() / (1024 * 1024));
+        break;
+    case SD_CARD_NONE:
+        Serial.println("BRAK KARTY");
+        break;
+    case SD_ERROR:
+        Serial.println("BŁĄD");
+        break;
+    case SD_FILE_OK:
+        Serial.println("PLIK OK");
+        break;
+    case SD_FILE_ERROR:
+        Serial.println("BŁĄD PLIKU");
+        break;
+
+    default:
+        Serial.println("BŁĄD");
+        break;
+    }
+}
